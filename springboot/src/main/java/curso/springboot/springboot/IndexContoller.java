1dfd660e91a691cc70a91afcec6a65cc392a437f
danilo.franco <danilo.franco@ILUMI-NB0002.ilumi.local>
2020-10-05 13:44:40 -0300

    Primeiro Commit

---
+++ springboot/src/main/java/curso/springboot/springboot/IndexContoller.java
@@ -0,0 +1,13 @@
+package curso.springboot.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexContoller {
+
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+}
