f5920f1f31438e8b3286974bc007a902a8062267
danilo.franco <danilo.franco@ILUMI-NB0002.ilumi.local>
2020-10-08 15:13:18 -0300

    Segundo Commit

--- springboot/src/main/java/curso/springboot/controller/PessoaController.java
+++ springboot/src/main/java/curso/springboot/controller/PessoaController.java
@@ -1,9 +1,15 @@
 package curso.springboot.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import curso.springboot.model.Pessoa;
@@ -11,33 +17,70 @@ import curso.springboot.repository.PessoaRepository;
 
 @Controller
 public class PessoaController {
-	
+
 	@Autowired
 	private PessoaRepository pessoaRepository;
 
-	@RequestMapping(method=RequestMethod.GET, value = "/cadastropessoa")
-	public String inicio() {
-		return "cadastro/cadastropessoa";
+	@RequestMapping(method = RequestMethod.GET, value = "/cadastropessoa")
+	public ModelAndView inicio() {
+		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+		modelAndView.addObject("pessoas", pessoasIt);
+		modelAndView.addObject("pessoaobj", new Pessoa());
+		return modelAndView;
 	}
-	
-	@RequestMapping(method = RequestMethod.POST, value= "/salvarpessoa")
-	public ModelAndView salvar (Pessoa pessoa) {
+
+	@RequestMapping(method = RequestMethod.POST, value = "**/salvarpessoa")
+	public ModelAndView salvar(Pessoa pessoa) {
 		pessoaRepository.save(pessoa);
-		
+
 		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
 		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
-		andView.addObject("pessoas",pessoasIt);
-		
-		return  andView;
+		andView.addObject("pessoas", pessoasIt);
+		andView.addObject("pessoaobj", new Pessoa());
+
+		return andView;
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/listapessoas")
+	public ModelAndView pessoas() {
+
+		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+		andView.addObject("pessoas", pessoasIt);
+		andView.addObject("pessoaobj", new Pessoa());
+		return andView;
+	}
+
+	@GetMapping("/editarpessoa/{idpessoa}")
+	public ModelAndView editar(@PathVariable("idpessoa") Long idpessoa) {
+		Optional<Pessoa> pessoa = pessoaRepository.findById(idpessoa);
+
+		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+		modelAndView.addObject("pessoaobj", pessoa.get());
+		return modelAndView;
+
+	}
+
+	@GetMapping("/excluirpessoa/{idpessoa}")
+	public ModelAndView excluir(@PathVariable("idpessoa") Long idpessoa) {
+		pessoaRepository.deleteById(idpessoa);
+
+		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+		andView.addObject("pessoas", pessoasIt);
+		andView.addObject("pessoaobj", new Pessoa());
+
+		return andView;
+
 	}
 	
-	@RequestMapping (method = RequestMethod.GET, value = "/listapessoas")
-	public ModelAndView pessoas() {
-		
-		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
-		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
-		andView.addObject("pessoas",pessoasIt);
-		return andView;
-		
+	@PostMapping("**/pesquisarpessoa")
+	public ModelAndView pesquisar(@RequestParam("nomepesquisa") String nomepesquisa) {
+		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+		modelAndView.addObject("pessoas",pessoaRepository.findPessoaByName(nomepesquisa));
+		modelAndView.addObject("pessoaobj", new Pessoa());
+		return modelAndView;
 	}
+
 }
--- springboot/src/main/java/curso/springboot/repository/PessoaRepository.java
+++ springboot/src/main/java/curso/springboot/repository/PessoaRepository.java
@@ -1,13 +1,17 @@
 package curso.springboot.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
+import java.util.List;
 import curso.springboot.model.Pessoa;
 
 @Repository
 @Transactional
 public interface PessoaRepository extends CrudRepository<Pessoa, Long> {
 
+	@Query("select p from Pessoa p where p.nome like %?1%")
+	List<Pessoa> findPessoaByName(String nome);
 }
