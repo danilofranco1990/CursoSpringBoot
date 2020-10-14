d68db8a7b792d5812d546a6e3359f7ddca6d8b01
danilo.franco <danilo.franco@ILUMI-NB0002.ilumi.local>
2020-10-14 12:28:51 -0300

    Commit 3

--- springboot/src/main/java/curso/springboot/controller/PessoaController.java
+++ springboot/src/main/java/curso/springboot/controller/PessoaController.java
@@ -1,9 +1,16 @@
 package curso.springboot.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,13 +20,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import curso.springboot.model.Pessoa;
+import curso.springboot.model.Telefone;
 import curso.springboot.repository.PessoaRepository;
+import curso.springboot.repository.TelefoneRepository;
 
 @Controller
 public class PessoaController {
 
 	@Autowired
 	private PessoaRepository pessoaRepository;
+	
+	@Autowired
+	private TelefoneRepository telefoneRepository;
 
 	@RequestMapping(method = RequestMethod.GET, value = "/cadastropessoa")
 	public ModelAndView inicio() {
@@ -31,7 +43,23 @@ public class PessoaController {
 	}
 
 	@RequestMapping(method = RequestMethod.POST, value = "**/salvarpessoa")
-	public ModelAndView salvar(Pessoa pessoa) {
+	public ModelAndView salvar(@Valid Pessoa pessoa, BindingResult bindingResult) {
+		
+		if(bindingResult.hasErrors()) {
+			ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+			Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+			modelAndView.addObject("pessoas", pessoasIt);
+			modelAndView.addObject("pessoaobj", pessoa);
+			
+			List<String> msg = new ArrayList<String>();
+			for(ObjectError objectError : bindingResult.getAllErrors()) {
+				msg.add(objectError.getDefaultMessage());// Vem das anotações @Empty @NotNull
+			}
+			modelAndView.addObject("msg", msg);
+			
+			return modelAndView;
+
+		}
 		pessoaRepository.save(pessoa);
 
 		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
@@ -49,6 +77,7 @@ public class PessoaController {
 		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
 		andView.addObject("pessoas", pessoasIt);
 		andView.addObject("pessoaobj", new Pessoa());
+
 		return andView;
 	}
 
@@ -59,7 +88,6 @@ public class PessoaController {
 		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
 		modelAndView.addObject("pessoaobj", pessoa.get());
 		return modelAndView;
-
 	}
 
 	@GetMapping("/excluirpessoa/{idpessoa}")
@@ -72,7 +100,6 @@ public class PessoaController {
 		andView.addObject("pessoaobj", new Pessoa());
 
 		return andView;
-
 	}
 	
 	@PostMapping("**/pesquisarpessoa")
@@ -80,6 +107,65 @@ public class PessoaController {
 		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
 		modelAndView.addObject("pessoas",pessoaRepository.findPessoaByName(nomepesquisa));
 		modelAndView.addObject("pessoaobj", new Pessoa());
+		return modelAndView;
+	}
+	
+	@GetMapping("/telefones/{idpessoa}")
+	public ModelAndView telefones(@PathVariable("idpessoa") Long idpessoa) {
+		Optional<Pessoa> pessoa = pessoaRepository.findById(idpessoa);
+		pessoaRepository.findById(idpessoa).get();
+
+		ModelAndView modelAndView = new ModelAndView("cadastro/telefones");
+		modelAndView.addObject("pessoaobj", pessoa.get());
+		modelAndView.addObject("telefones", telefoneRepository.getTelefones(idpessoa));
+
+		
+		return modelAndView;
+	}
+	
+	@PostMapping("**/addfonePessoa/{pessoaid}")
+	public ModelAndView addFonePessoa(Telefone telefone, @PathVariable("pessoaid")Long pessoaid) {
+		
+		Pessoa pessoa = pessoaRepository.findById(pessoaid).get();
+		
+		if(telefone != null &&  telefone.getNumero().isEmpty() ||  telefone.getTipo().isEmpty() ) {
+			
+			ModelAndView modelAndView = new ModelAndView("cadastro/telefones");
+			modelAndView.addObject("pessoaobj",pessoa);
+			modelAndView.addObject("telefones", telefoneRepository.getTelefones(pessoaid));
+			List<String> msg = new ArrayList<String>();
+			
+			if(telefone.getNumero().isEmpty()) {
+				msg.add("Número deve ser informado");
+			}
+			if(telefone.getTipo().isEmpty()) {
+				msg.add("Tipo deve ser informado");
+			}
+			
+			modelAndView.addObject("msg",msg);
+			
+			return modelAndView;
+		}
+		
+		telefone.setPessoa(pessoa);
+		telefoneRepository.save(telefone);
+		
+		ModelAndView modelAndView = new ModelAndView("cadastro/telefones");
+		modelAndView.addObject("pessoaobj",pessoa);
+		modelAndView.addObject("telefones", telefoneRepository.getTelefones(pessoaid));
+		return modelAndView;
+	}
+	
+	@GetMapping("**/excluirtelefone/{idtelefone}")
+	public ModelAndView excluirTelefone(@PathVariable("idtelefone") Long telefoneid) {
+		Pessoa pessoa = telefoneRepository.findById(telefoneid).get().getPessoa();
+		telefoneRepository.deleteById(telefoneid);
+
+		ModelAndView modelAndView = new ModelAndView("cadastro/telefones");
+		modelAndView.addObject("pessoaobj",pessoa);
+		modelAndView.addObject("telefones", telefoneRepository.getTelefones(pessoa.getId()));
+
+
 		return modelAndView;
 	}
 
--- springboot/src/main/java/curso/springboot/model/Pessoa.java
+++ springboot/src/main/java/curso/springboot/model/Pessoa.java
@@ -1,12 +1,20 @@
 package curso.springboot.model;
 
-
 import java.io.Serializable;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.websocket.OnOpen;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import antlr.collections.List;
 
 @Entity // Toda Tabela que será criada no banco de dados tem que ser anotada com a anotação @Entity
 public class Pessoa implements Serializable {
@@ -17,12 +25,29 @@ public class Pessoa implements Serializable {
 	@Id
 	@GeneratedValue (strategy=GenerationType.AUTO)
 	private Long id;
+	
+	@NotEmpty(message = "Nome não pode ser vazio")
+	@NotNull (message = "Nome não pode ser nulo")
 	private String nome;
+	
+	@NotEmpty(message = "Sobrenome não pode ser vazio")
+	@NotNull (message = "Sobrenome não pode ser nulo")
 	private String sobrenome;
-	private int idade;
+	
+	@Min(value = 18, message = "Idade inválida")
+	private int idade;	
+	
+	@OneToMany(mappedBy = "pessoa", orphanRemoval = true, cascade = CascadeType.ALL)
+	private java.util.List<Telefone> telefones;
 	
 	
 	
+	public java.util.List<Telefone> getTelefones() {
+		return telefones;
+	}
+	public void setTelefones(java.util.List<Telefone> telefones) {
+		this.telefones = telefones;
+	}
 	public int getIdade() {
 		return idade;
 	}
